edf52cec2b9306ef0bfcc7b1d805af48554f168e
David <david-bitner@hotmail.com>
2024-06-10 19:04:28 -0300

    Footer and minor changes

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,9 @@ var elem = document.querySelector(".main-carousel");
 var flkty = new Flickity(elem, {
   // options
   wrapAround: true,
-  autoPlay: 8000,
+  autoPlay: 10000,
   selectedAttraction: 0.01,
   friction: 1,
   draggable: false,
+  pauseAutoPlayOnHover: false,
 });
