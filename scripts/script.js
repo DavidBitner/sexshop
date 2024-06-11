f628b6328754107507129f8af3c543a36788a141
David <david-bitner@hotmail.com>
2024-06-11 16:47:22 -0300

    Update index.html
    Added testimonials section and some minor changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 //Carousels
-var elem = document.querySelector(".main-carousel");
-var flkty = new Flickity(elem, {
+var carouselMain = document.querySelector(".main-carousel");
+var carouselTestimonials = document.querySelector(`.testimonials__carousel`)
+
+var flkty1 = new Flickity(carouselMain, {
   // options
   wrapAround: true,
   autoPlay: 10000,
@@ -8,6 +10,15 @@ var flkty = new Flickity(elem, {
   friction: 1,
   draggable: false,
   pauseAutoPlayOnHover: false,
+});
+
+var flkty2 = new Flickity(carouselTestimonials, {
+  wrapAround: true,
+  autoPlay: 4000,
+  selectedAttraction: 0.01,
+  friction: 1,
+  prevNextButtons: false,
+  pageDots: false,
 });
 
 //Keep scroll in one place on reload
