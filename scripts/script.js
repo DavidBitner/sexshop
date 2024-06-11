b2fe1fa09de9aefb850d3b7b78e6e1640c07d2f0
David <david-bitner@hotmail.com>
2024-06-11 15:56:17 -0300

    Update index.html
    Added some SVG's, created sections, finished shipping section and made it responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//Carousels
 var elem = document.querySelector(".main-carousel");
 var flkty = new Flickity(elem, {
   // options
@@ -8,3 +9,13 @@ var flkty = new Flickity(elem, {
   draggable: false,
   pauseAutoPlayOnHover: false,
 });
+
+//Keep scroll in one place on reload
+document.addEventListener("DOMContentLoaded", function (event) {
+  var scrollpos = localStorage.getItem("scrollpos");
+  if (scrollpos) window.scrollTo(0, scrollpos);
+});
+
+window.onbeforeunload = function (e) {
+  localStorage.setItem("scrollpos", window.scrollY);
+};
